cfabb5a102051f023a1bbdd77e760469740b843c
testholder <184759249+testholder@users.noreply.github.com>
2026-05-05 12:22:10 +0100

    handle messages with prefix TODO: create loadCommands

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
-const { Client, Events, GatewayIntentBits } = require("discord.js");
+const { Client, Events, GatewayIntentBits, Collection} = require("discord.js");
 require('dotenv').config();
+
+const PREFIX = process.env.DEFAULT_PREFIX;
 
 const client = new Client({
     intents: [
@@ -15,8 +17,46 @@ const client = new Client({
     ]
 });
 
+client.commands = new Collection();
+
+
+// help functions
+async function loadCommands() {
+    console.log("commands loaded");
+}
+
+// bot events
+client.on(Events.MessageCreate, async (message) => {
+    if (message.author.bot) return;
+    if (!message.content.startsWith(PREFIX)) return;
+    
+    // remove prefix and split message into a array
+    const args = message.content.slice(PREFIX.length).trim().split(/\s+/);
+    
+    // remove first index of array which is command
+    const commandName = args.shift().toLowerCase();
+    
+
+    console.log(args);
+    console.log(commandName);
+
+    let command = client.commands.get(commandName);
+    if (!command) return;
+    
+    try{ 
+        // handle command
+    } catch (error) {
+        console.error("error executing command: ", error);
+    }
+
+})
+
 client.once(Events.ClientReady, (client) => {
-	console.log(`Ready! Logged in as ${client.user.tag}`);
+	console.log(`Logged in as ${client.user.tag}`);
 });
 
-client.login(process.env.BOT_TOKEN);
+
+(async () => {
+    await loadCommands();
+    client.login(process.env.BOT_TOKEN);
+})();
